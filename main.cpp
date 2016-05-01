48164d7b114aafa8d0d867046003f5b35ea6807d
tomasz.chrosniak <tomasz.chrosniak@gmail.com>
2016-05-01 09:44:24 +0100

    Demo application added. Found a precision bug, which was expected, but it affects the results of the operations undo method. Will have to change how undo returns to the previous value.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,98 @@
 #include <iostream>
 #include <Calculator.h>
+#include <string>
+#include <iomanip>
 
-int main()
+using namespace std;
+
+double value;
+
+int getValueFromCommandString(const int &offset, const string &input,  int &numberOfCharsRead)
 {
-	std::cout << "Hello World!\n";
+	return sscanf_s(input.substr(offset, input.length() - offset).c_str(), "%lf%n", &value, &numberOfCharsRead);
+}
+
+int main(int argc, char **argv)
+{
+	Calculator calc;
+	string commandString;
+	int numberOfCharactersRead;
+	cout << setprecision(std::numeric_limits<double>::digits10 + 1);
+	while (true)
+	{
+		cout << "Current total: " << calc.getCurrentTotal() << endl;
+		cin >> commandString;
+		for (unsigned int i = 0; i < commandString.length(); ++i)
+		{
+			switch (commandString.at(i))
+			{
+			case '+':
+				i++;
+				if (getValueFromCommandString(i, commandString, numberOfCharactersRead) > 0)
+				{
+					calc.addValue(value);
+					i += (numberOfCharactersRead-1);
+				}
+				break;
+			case '-':
+				i++;
+				if (getValueFromCommandString(i, commandString, numberOfCharactersRead) > 0)
+				{
+					calc.subtractValue(value);
+					i += (numberOfCharactersRead - 1);
+				}
+				break;
+			case '*':
+				i++;
+				if (getValueFromCommandString(i, commandString, numberOfCharactersRead) > 0)
+				{
+					calc.multiplyBy(value);
+					i += (numberOfCharactersRead - 1);
+				}
+				break;
+			case 'x':
+				i++;
+				if (getValueFromCommandString(i, commandString, numberOfCharactersRead) > 0)
+				{
+					calc.multiplyBy(value);
+					i += (numberOfCharactersRead - 1);
+				}
+				break;
+			case '/':
+				i++;
+				if (getValueFromCommandString(i, commandString, numberOfCharactersRead) > 0)
+				{
+					try {
+						calc.divideBy(value);
+					}
+					catch (Calculator::DivisionByZeroException e)
+					{
+						std::cerr << e.what() << endl;
+					}
+					i += (numberOfCharactersRead - 1);
+				}
+				break;
+			case 'h':
+				cout << "Displaying command history:" << endl << calc.getOperationsString();
+				break;
+			case 'c':
+				cout << "Clearing history." << endl;
+				calc.clear();
+				break;
+			case 'u':
+				calc.undo();
+				break;
+			case 'q':
+				return 0;
+			default:
+				if (getValueFromCommandString(i, commandString, numberOfCharactersRead) > 0)
+				{
+					calc.clear();
+					calc.addValue(value);
+					i += (numberOfCharactersRead-1);
+				}
+				break;
+			}
+		}
+	}
 }
